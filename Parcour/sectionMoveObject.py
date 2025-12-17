c0bacca108e1edd057866a1d8a599cf708285054
md <micha_duerr@protonmail.com>
2025-12-17 16:06:30 +0100

    SectionMoveObject funktioniert

--- Parcour/sectionMoveObject.py
+++ Parcour/sectionMoveObject.py
@@ -12,9 +12,9 @@ class SectionMoveObject(Section):
         self.name = "Move Object"
         self.target_distance = 180 # mm Abstand zur Wand links 
         #TODO Distanz vllt näher ran, damit Sensor genauer wird????
-        self.speed = 120 # Vorwärtsgeschwindigkeit
+        self.speed = 280 # Vorwärtsgeschwindigkeit
         self.kp = 0.9 # P-Gain
-        self.kd = 0.3 # D-Gain
+        self.kd = 0.20 # D-Gain
         self.started = False
 
         self.last_error = 0
@@ -55,6 +55,7 @@ class SectionMoveObject(Section):
             robot.calibrate_gripper_and_ultrasonic_angle()
             robot.reset_distance_and_angle()
             robot.drive_base.drive_time(120, 0, 1000)
+            robot.base_rgb = robot.color_sensor.rgb()
             self.goto_state(1)
             
         if self.state == 1:
@@ -72,7 +73,7 @@ class SectionMoveObject(Section):
             steering = self.kp * error + self.kd * d_error
         
             steering = max(min(steering, self.STEERING), -self.STEERING)
-            self.update_section_screen(robot, status= str(dist) + " mm", status2="Error:" + str(error), status3="Steering: " + str(steering))
+            self.update_section_screen(robot, status="pd-Regler", status2="", status3="")
         #2
             robot.drive_base.drive(self.speed, -steering)
         
@@ -85,6 +86,7 @@ class SectionMoveObject(Section):
                 robot.drive_base.stop()
                 self.update_section_screen(robot, status="firstPartDone", status2="", status3="")
                 if self.findBlue:
+
                     self.goto_state(7)  # Move to find blue
                 elif self.turned:
                     self.goto_state(4)
@@ -99,7 +101,7 @@ class SectionMoveObject(Section):
                 pass
             robot.reset_distance_and_angle()
             self.distance_travelled = 0
-            self.target_distance = 100
+            self.target_distance = 110
             self.max_distance = 260 # weniger als bis zum Objekt um dann farbsuche zu starten
             self.turned = True
             self.goto_state(1)
@@ -124,11 +126,11 @@ class SectionMoveObject(Section):
 
         if self.state == 5:
             #robot.drive_base.drive_time(-120, -40, 3000)
-            robot.drive(-150, -40)
+            robot.drive(-285, -40)
             while robot.angle_turned() > -90:
                 pass
             robot.stop()
-            ronot.straight(-200)
+            robot.straight(-200)
             robot.reset_distance_and_angle()
             robot.set_gripper_and_ultrasonic_angle(self.GRIPPINGANGLE, turn_speed=100)
             robot.straight(-100)
@@ -136,30 +138,73 @@ class SectionMoveObject(Section):
             self.goto_state(6)
 
         if self.state == 6:
-            robot.spin(-50)
+            #robot.spin(100)
+            robot.drive(30, 30)
+            while robot.angle_turned() < 145:
+                pass
             robot.reset_distance_and_angle()
             self.distance_travelled = 0
-            self.target_distance = 300
+            self.target_distance = 330
             self.max_distance = 400
             self.findBlue = True
-            self.goto_state(1)
+            self.goto_state(7)
+
 
         if self.state == 7:
-            #Farbe lesen
-            detectedColor = robot.color_sensor.color()
-            self.update_section_screen(robot, status=str(detectedColor), status2="Auf Blau warten", status3="")
+            dist = robot.ultrasonic_sensor.distance()
+            print(dist)
+            robot.spin(15)
+            dist2 = robot.ultrasonic_sensor.distance()
+            print("Zweite", dist2)
+            self.update_section_screen(robot, status=str(dist), status2=str(dist2) , status3="")
+            while dist2 < dist:
+                dist = dist2
+                robot.spin(15)
+                dist2 = robot.ultrasonic_sensor.distance()
+                print("Zweite", dist2)
 
-            #Fahren bis Blau erkannt wird
-            robot.drive_base.drive(self.speed, 0)
-            if detectedColor == Color.BLUE:
-                robot.drive_base.stop()
-                self.goto_state(8)
-
+            robot.ev3.speaker.beep()
+            self.goto_state(8)
+            
         if self.state == 8:
-            # beep
-            robot.sound.beep()
-            self.update_section_screen(robot, status="Section Finished", status2="", status3="")
+            dist = robot.ultrasonic_sensor.distance()
+            detectedColor = robot.color_sensor.rgb()
+            if dist is None:    #falls Sensor nichts erkennt
+                dist = self.target_distance 
+            
+            error = dist - self.target_distance
+            now = time.time()
+            dt = now - self.last_time if now != self.last_time else 0.01
+            dt = max(dt, 0.01)  # enforce minimum dt threshold
+        
+            d_error = (error - self.last_error) / dt
+            #steering = self.kp * error
+            steering = self.kp * error + self.kd * d_error
+        
+            steering = max(min(steering, self.STEERING), -self.STEERING)
+            self.update_section_screen(robot, status= str(dist) + " mm", status2="Error:" + str(error), status3="Steering: " + str(steering))
+        #2
+            robot.drive_base.drive(self.speed / 4, -steering)
+        
+        
+            self.last_error = error
+            self.last_time = now
+            #self.distance_travelled += self.speed * dt
+         
+            self.update_section_screen(robot, status=str(detectedColor), status2="Auf Blau warten", status3="")
+            base_r, base_g, base_b = robot.base_rgb
+            relative_blue_change = (detectedColor[2] - base_b) / max(base_b, 1)
+            if relative_blue_change > 3:
+                detectedColor = Color.BLUE
+                robot.drive_base.stop()
+                self.goto_state(9)
 
+        if self.state == 9:
+            # beep
+            robot.ev3.speaker.beep()
+            robot.stop()
+            self.update_section_screen(robot, status="Section Finished", status2="", status3="")
+            self.goto_state(10)
 
 
 
